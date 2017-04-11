d8b28ef6c8e67efc3579d075e99661c78df25a32
genu.wb <genu.wb@alipay.com>
2017-04-12 01:05:33 +0800

    添加修改阅背景

---
+++ app/src/main/java/com/codingbingo/fastreader/ui/adapter/ReadingBackgroundAdapter.java
@@ -0,0 +1,29 @@
+package com.codingbingo.fastreader.ui.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Author: bingo
+ * Email: codingbingo@gmail.com
+ * By 2017/4/12.
+ *
+ * 阅读背景设置
+ */
+
+public class ReadingBackgroundAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
